2a13bc9027de70591a60551ccd13fa3b7955e0d1
Hans Westerbeek <hanswesterbeek@gmail.com>
2012-11-02 11:26:05 +0100

    solutions

--- src/test/groovy/enhance/nlgug/ProductServiceSpec.groovy
+++ src/test/groovy/enhance/nlgug/ProductServiceSpec.groovy
@@ -21,31 +21,46 @@ class ProductServiceSpec extends Specification {
 
 		productService = new ProductServiceImpl(productRepository: productRepository, emailer: emailer)
 
-		product = new Product()
+		product = new Product(id:  14, name: "foo", stock: 10)
 	}
 
 
 
 	def "Retrieving a Product happens by using the id that you pass to the service "(){
-		given:
-			def id = 1
+
 		when:
-			def product = productService.findProduct(id)
+			def result = productService.findProduct(product.id)
 		then:
-			1 * productRepository.getProduct(id)
+			result == product
+			1 * productRepository.getProduct(product.id) >> {return product}
+
+
 	}
 
 	def "Successfully add a product"(){
-
-
+		when:
+			productService.addProduct(product)
+		then:
+			1 * productRepository.findByName(product.name) >> { return null}
+			1 * productRepository.addProduct(product)
 	}
 
 	def "Can not add a product with a name that already exists"(){
-
+		when:
+			productService.addProduct(product)
+		then:
+			1 * productRepository.findByName(product.name) >> { return new Product()} // we dont really care about the actual product returned
+			0 * productRepository.addProduct(_) // that method may not have been invoked
 	}
 
 	def "Selling a product causes its stock to be updated"(){
-
+		given:
+			def quantity = 3
+		when:
+			productService.sell(product.id, quantity)
+		then:
+			1 * productRepository.getProduct(product.id) >> {return product}
+			product.stock == 7
 	}
 
 	def "An email about our product must be sent when has hit the stock limit"(){
